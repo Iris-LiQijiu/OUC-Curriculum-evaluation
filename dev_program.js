c42d2733b989d64edb9dac491729028d4db74091
Iris-LiQijiu <74090083+Iris-LiQijiu@users.noreply.github.com>
2020-11-15 21:05:50 +0800

    Add files via upload

---
+++ dev_program.js
@@ -0,0 +1,23 @@
+// pages/dev_program/dev_program.js
+Page({
+  data: {
+    math_list: [
+      {
+        id: "0",
+        txt: "计算机"
+      },
+      {
+        id: "1",
+        txt: "电子"
+      },
+      {
+        id: "2",
+        txt: "海技"
+      },
+      {
+        id: "3",
+        txt: "物理"
+      },
+    ],
+  },
+})
